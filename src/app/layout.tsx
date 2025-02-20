7e581876719ecb80614996b92fecd08c5f46f2b5
Famgz <famgz@proton.me>
2025-02-20 17:29:56 -0300

    chore: add project font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,16 @@
 import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
 import './globals.css';
 
 export const metadata: Metadata = {
   title: 'FSW Donalds',
   description: 'Fast Food App',
 };
+
+const font = Poppins({
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  subsets: ['latin'],
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +19,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body>{children}</body>
+      <body className={`${font.className}`}>{children}</body>
     </html>
   );
 }
